142961b51d49c62a3f16be98fbd574e4af2114bc
Matthew Todor <unclejunglegeorge@gmail.com>
2022-02-19 21:12:16 -0700

    added goals to js comment

---
+++ script.js
@@ -0,0 +1,6 @@
+// access API
+//when searching for a city, weather info is shown for that city
+//Info to be shown: city name, date, icon of weather condition, tep, humidity, weind speed, and UV index
+//uv index should have color code based on severity of UV
+//future weather conditons for city needs : date, icon, temp, wind, and humidity
+//all searched cities should be accessible in history, and pull up same information again.
